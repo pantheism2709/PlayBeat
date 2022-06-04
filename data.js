de44b88288d8c44e68af1a8bf326d83b61e80092
sachin2709 <sachinmishra2709@gmail.com>
2022-06-04 06:55:55 +0530

    adding files

---
+++ data.js
@@ -0,0 +1,73 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'assets/musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'assets/images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'assets/musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'assets/images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'assets/musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'assets/images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'assets/musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'assets/images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'assets/musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'assets/images/cover 5.png'
+    },
+    {
+        name: 'song 6',
+        path: 'assets/musics/Song 6.mp3',
+        artist: 'artist 6',
+        cover: 'assets/images/cover 6.png'
+    },
+    {
+        name: 'song 7',
+        path: 'assets/musics/Song 7.mp3',
+        artist: 'artist 7',
+        cover: 'assets/images/cover 7.png'
+    },
+    {
+        name: 'song 8',
+        path: 'assets/musics/Song 8.mp3',
+        artist: 'artist 8',
+        cover: 'assets/images/cover 8.png'
+    },
+    {
+        name: 'Asal Mein',
+        path: 'assets/musics/Asal Mein - Darshan Raval.mp3',
+        artist: 'darshan Raval',
+        cover: 'assets/images/asal-mein-darshan-raval-250.jpg'
+    },
+    {
+        name: 'Hawa Banke',
+        path: 'assets/musics/Hawa Banke - Darshan Raval.mp3',
+        artist: 'Darshan Raval',
+        cover: 'assets/images/hawa-banke-darshan-raval-250.jpg'
+    },
+    {
+        name: 'Ek Tarfa',
+        path: 'assets/musics/Ek Tarfa - Darshan Raval.mp3',
+        artist: 'Darshan Raval',
+        cover: 'assets/images/ek-tarfa-darshan-raval-250.jpg'
+    },
+]
+
+
+
+
+
